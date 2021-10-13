e0d352cdeed83f1f616b253759010ef83344c10b
David Lowry <DavidLowry64@gmail.com>
2021-10-13 16:59:07 -0400

    Initial Commit

---
+++ SpringBoot103/src/main/java/com/dgl/helloWorld/HomeController.java
@@ -0,0 +1,14 @@
+package com.dgl.helloWorld;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+
+@Controller
+public class HomeController {
+@RequestMapping("/")
+public String homePage() {
+	return "homeTemplate";
+}
+}
