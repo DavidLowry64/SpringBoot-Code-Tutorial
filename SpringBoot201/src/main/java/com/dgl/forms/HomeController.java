61de2df918d75db41ec57039d0b1d8678ac8da53
David Lowry <DavidLowry64@gmail.com>
2021-10-15 10:06:27 -0400

    Initial Commit

---
+++ SpringBoot201/src/main/java/com/dgl/forms/HomeController.java
@@ -0,0 +1,26 @@
+package com.dgl.forms;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;  
+
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/loadform")
+	public String loadFormPage() {
+		return "formtemplate";
+	}
+	
+	
+	@RequestMapping("/processform")
+	public String loadFormPage(@RequestParam("login") String login, Model model)
+	{
+		model.addAttribute("loginval", login);
+	return "confirm";	
+	}
+}
---
+++ SpringBoot201/src/test/java/com/dgl/forms/SpringBoot201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgl.forms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
